2cd7bd5cd5495a517a403aa40e11cbe2a7ad9d74
Elvisther Miranda <elvisther_cs@hotmail.com>
2022-08-19 15:41:48 -0400

    update seeder create user

--- AppInfluenciou/database/seeders/RolesAndPermissionsSeeder.php
+++ AppInfluenciou/database/seeders/RolesAndPermissionsSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 use Spatie\Permission\Models\Role;
 
 class RolesAndPermissionsSeeder extends Seeder
@@ -22,5 +24,13 @@ class RolesAndPermissionsSeeder extends Seeder
         $role = Role::create(['name' => 'instrutor']);
         $role = Role::create(['name' => 'admin']);
         $role = Role::create(['name' => 'super-admin']);
+
+        $user = User::create([
+            'name' => 'Admin',
+            'email' => 'admin@influenciou.com.br',
+            'password' => Hash::make('admin@influenciou2022', ['cost' => 12])
+        ]);
+
+        $user->assingRole('admin');
     }
 }
