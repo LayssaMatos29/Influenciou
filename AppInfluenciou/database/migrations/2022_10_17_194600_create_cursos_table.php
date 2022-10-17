f2a2512990ab61c99f6c60f7cc868df44353ec0e
elvisther.miranda <elvisther_cs@hotmail.com>
2022-10-17 19:07:19 -0400

    criado migrations

---
+++ AppInfluenciou/database/migrations/2022_10_17_194600_create_cursos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cursos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->longText('descricao');
+            $table->foreignId('categoria_id')->constrained('categorias');
+            $table->foreignId('instrutor_id')->constrained('users');
+            $table->decimal('preco', $precision = 8, $scale = 2);
+            $table->enum('disponivel', ['S', 'N']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cursos');
+    }
+};
---
+++ AppInfluenciou/database/migrations/2022_10_17_200242_create_inscricoes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('inscricoes', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->foreignId('curso_id')->constrained('cursos');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('inscricoes');
+    }
+};
