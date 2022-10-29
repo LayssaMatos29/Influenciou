8eb0ac477eaf1dce0f45e577bc56534eb9f11a51
elvisther.miranda <elvisther_cs@hotmail.com>
2022-10-28 20:34:41 -0400

    rotas de modulos e conteudos criados

--- AppInfluenciou/routes/instrutor.php
+++ AppInfluenciou/routes/instrutor.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\Instrutor\Curso\ConteudoController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Instrutor\Curso\CursoController;
+use App\Http\Controllers\Instrutor\Curso\ModuloController;
 
 Route::group([
     'prefix' => 'instrutor',
@@ -12,4 +14,18 @@ Route::group([
     })->name('dashboard.instrutor.index');
 
     Route::resource('/cursos', CursoController::class);
+
+    Route::get('/modulos', [ModuloController::class, 'index'])->name('curso.modulo.index');
+    Route::get('/modulos/{id}', [ModuloController::class, 'index'])->name('curso.modulo.show');
+    Route::get('/modulos/create', [ModuloController::class, 'create'])->name('curso.modulo.create');
+    Route::post('/modulos', [ModuloController::class, 'store']);
+    Route::get('/modulos/{id}/edit', [ModuloController::class, 'edit'])->name('curso.modulo.edit');
+    Route::put('/modulos/{id}', [ModuloController::class, 'update'])->name('curso.modulo.update');
+
+    Route::get('/conteudo', [ConteudoController::class, 'index'])->name('curso.conteudo.index');
+    Route::get('/conteudo/{id}', [ConteudoController::class, 'index'])->name('curso.conteudo.show');
+    Route::get('/conteudo/create', [ConteudoController::class, 'create'])->name('curso.conteudo.create');
+    Route::post('/conteudo', [ConteudoController::class, 'store']);
+    Route::get('/conteudo/{id}/edit', [ConteudoController::class, 'edit'])->name('curso.conteudo.edit');
+    Route::put('/conteudo/{id}', [ConteudoController::class, 'update'])->name('curso.conteudo.update');
 });
